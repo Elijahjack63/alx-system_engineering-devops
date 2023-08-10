4cf7c1d11fa446b6b94454463b023a79398d1ac8
Elijahjack63 <elijahjack526@yahoo.com>
2023-08-10 02:02:49 +0000

    done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# returns 500; ans uses this script to fix typo in config
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
